9395296d43b11cdb7ad50bf08478c34e80a468ca
JaeHun Kim <kkaa81@naver.com>
2022-01-08 17:20:10 +0900

    fix(food): add food router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import userRouter from './auth.js';
+import foodRouter from './food.js';
 
 const rootRouter = express.Router();
 
 rootRouter.use('/auth', userRouter);
+rootRouter.use('/food', foodRouter);
 
 export default rootRouter;
 
