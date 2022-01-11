ff6628afe916924218000db4b50a0c5232c19a8b
JaeHun Kim <kkaa81@naver.com>
2022-01-11 20:16:19 +0900

    feat(validate): add validate rules

--- routes/auth.js
+++ routes/auth.js
@@ -1,91 +1,48 @@
 import express from 'express';
+import { body } from 'express-validator';
 
 import * as userRepository from '../data/auth.js';
 import * as userController from '../controller/auth.js';
+import { validate } from '../middlewares/validator.js';
 
 const userRouter = express.Router();
 
+const validateRegister = [
+    body('username')
+        .trim()
+        .notEmpty()
+        .withMessage('username을 입력해주세요.'),
+    body('password')
+        .trim()
+        .isLength({ min: 6 })
+        .withMessage('비밀번호는 최소 5글자입니다.'),
+    body('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('이메일으로 입력해주세요.'),
+    body('name')
+        .notEmpty()
+        .withMessage('이름을 입력해주세요.'),
+    validate
+]
 
-
-//post: 회원가입, 로그인, 아이디찾기, 비밀번호바꾸기
-/**
-* @swagger
-* paths:
-*  /auth/login:
-*   post:
-*     tags: [Auth]
-*     summary: 로그인
-*     parameters:
-*       - name: code
-*         in: Post
-*         type: string
-*         description: 로그인시 서버에 보낼 정보
-*         schema:
-*           $ref: '#/components/schemas/Login'
-*     responses:
-*       "200":
-*         discription: 로그인 성공
-*         contnet:
-*           application:json
-*       "400":
-*         discription: 잘못된 파라메타 전달
-*     
-*/
-
-/**
-* @swagger
-* paths:
-*  /auth/register:
-*   post:
-*     tags: [Auth]
-*     summary: 회원가입
-*     parameters:
-*       - name: code
-*         in: Post
-*         type: string
-*         description: 회원가입할때 보내줘야할 데이터
-*         schema:
-*           $ref: '#/components/schemas/Register'
-*     responses:
-*       "200":
-*         discription: 회원가입 성공
-*       "400":
-*         discription: 잘못된 파라메타 전달
-*       schema:
-*           $ref: 
-*/
-
-/**
-* @swagger
-* paths:
-*  /auth/:id/password:
-*   post:
-*     tags: [Auth]
-*     summary: 비밀번호 찾기
-*     parameters:
-*       - name: code
-*         in: Post
-*         type: string
-*         description: 비밀번호 찾기 할때 보내줄 데이터
-*         schema:
-*           $ref: '#/components/schemas/Find password'
-*     responses:
-*       "200":
-*         discription: 로그인 성공
-*         contnet:
-*           application:json
-*       "400":
-*         discription: 잘못된 파라메타 전달
-*/
+const validateFindPassword = [
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('바꾸실 비밀번호를 입력해주세요.'),
+    validate
+]
 
 
 userRouter.route('/')
     .get(async (req, res, next) => {
         res.send(await userRepository.getUsers());
     });
-userRouter.route('/login').post(userController.login);
-userRouter.route('/register').post(userController.createUser);
-userRouter.route('/:name/password').post(userController.changePassword);
+userRouter.route('/login').post(validate, userController.login);
+userRouter.route('/register').post(validateRegister, userController.createUser);
+userRouter.route('/:name/password').post(validateFindPassword, userController.changePassword);
 
 
 
