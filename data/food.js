853c404066b46800a29fe8a25603bc09be9b8dc9
JaeHun Kim <kkaa81@naver.com>
2022-01-19 17:50:44 +0900

    fix(data): change fake data

--- data/food.js
+++ data/food.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
+
+import { config } from '../config.js';
+
+const kakaoKey = config.key.kakaoKey;
+const header = {
+    header: {
+        Authorization: `KakaoAK ${kakaoKey}`
+    }
+}
+
+
 const stores = [
     { id: 1, name: '재훈이네 분식점', tag: 1, main: '제육 볶음' },
     { id: 2, name: '민선이네 치킨집', tag: 2, main: '양념 치킨' },
@@ -5,16 +17,14 @@ const stores = [
     { id: 4, name: '다몬이네 피자집', tag: 4, main: '포테이토 피자' },
 ];
 
-const tags = [
-    { id: 1, name: '제육' },
-    { id: 2, name: '치킨' },
-    { id: 3, name: '떡볶이' },
-    { id: 4, name: '피자' },
-    { id: 5, name: '보쌈' },
-]
+const tags = [];
 
 export async function getStores() {
     return stores;
+}
+
+export async function getTag() {
+    return tags;
 }
 
 export async function getTags() {
@@ -28,4 +38,45 @@ export async function findTagId(foodName) {
 export async function findStore(recommend, food) {
     // return recommend.filter(store => !store.tag.includes(selectedFood[i]));
     return recommend.filter(store => store.tag !== food.id);
+}
+
+export async function findStoreApi(page, longitude, latitude) {
+    return axios({
+        url:'https://dapi.kakao.com/v2/local/search/category.json',
+        method:"GET",
+        headers:{"Authorization":`KakaoAK ${kakaoKey}`},
+        params:{
+            category_group_code: 'FD6',
+            rect: `${latitude - 0.0005},${longitude - 0.0005},${latitude + 0.0005},${longitude + 0.0005}`,
+            page: page,
+        }
+    });
+
+    37.5016, 127.0262
+}
+
+export async function restyle(data) {
+    return data.map((store, i) => {
+        // console.log(store);
+        const category = store['category_name'].split('>');
+        if (category.length > 2) {
+            const restyled = { 
+                id: i,
+                address: store['address_name'],
+                tag: category[category.length - 1].trim(),
+                kind: category[1].trim(),
+                name: store['place_name'],
+            };
+            // tags.push(restyled);
+            return restyled;
+        } else {
+            return {
+                id: i,
+                address: store['address_name'],
+                tag: category[category.length - 1].trim(),
+                kind: category[0].trim(),
+                name: store['place_name'],
+            }
+        }
+    })
 }
