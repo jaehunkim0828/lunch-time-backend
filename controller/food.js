2527b0a5f5fee02eba00eacac53ff441cdeda173
JaeHun Kim <kkaa81@naver.com>
2022-01-19 17:50:21 +0900

    feat(store): get seoul store

--- controller/food.js
+++ controller/food.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as foodRepository from '../data/food.js';
 
 export async function findStore(req, res, next) {
@@ -15,4 +17,24 @@ export async function findStore(req, res, next) {
 export async function getTags(req, res, next) {
     const tags = await foodRepository.getTags();
     return res.status(200).send(tags);
+}
+
+export async function getStore(req, res, next) {
+    const datas = [];
+    for (let i = 0; i < 10; i += 1) {
+        const [logitude, latitude] = [37.5016 + i * 0.001, 127.0262 + i * 0.001];
+        let countIndex = 1;
+        while(countIndex < 45) {
+            const current = await foodRepository.findStoreApi(countIndex, logitude, latitude);
+            datas.push(...current.data.documents);
+            console.log(datas.length);
+            console.log(current.data.meta);
+            if (current.data.meta.is_end) {
+                break;
+            };
+            countIndex += 1;
+        }
+    }
+    const result = await foodRepository.restyle(datas);
+    return res.send(result);
 }
--- routes/food.js
+++ routes/food.js
@@ -4,13 +4,9 @@ import * as foodController from '../controller/food.js';
 
 const foodRouter = express.Router();
 
-
-foodRouter.route('/').post(foodController.findStore); //선택한 음식 빼고 나머지 음식점 보내주기.
+foodRouter.route('/')
+.get(foodController.getStore)
+    .post(foodController.findStore) //선택한 음식 빼고 나머지 음식점 보내주기.
 foodRouter.route('/tag').get(foodController.getTags); // 모든 tag들 보내주기.
-
-foodRouter.route('/location')
-    .post((req, res, next) => {
-        //위치 1km반경 구하기
-    })
 
 export default foodRouter;
