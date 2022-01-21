bd9ecc0bb6b9e0b587ba6615470f8fd9b018f207
JaeHun Kim <kkaa81@naver.com>
2022-01-21 23:12:42 +0900

    fix(err): chage path

--- routes/food.js
+++ routes/food.js
@@ -8,6 +8,6 @@ foodRouter.route('/')
     .get(foodController.getStore)
     .post(foodController.findStore) //선택한 음식 빼고 나머지 음식점 보내주기.
 foodRouter.route('/tag').get(foodController.getTags); // 모든 tag들 보내주기.
-foodRouter.route('api/name').get(foodController.getFoodName);
+foodRouter.route('/api/name').get(foodController.getFoodName);
 
 export default foodRouter;
