07da2e4e25fddf5a4aa5faa8f8459aff1df0922c
JaeHun Kim <kkaa81@naver.com>
2022-02-22 21:34:59 +0900

    stop express/validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { body } from 'express-validator';
+// import { body } from 'express-validator';
 
 import * as userRepository from '../data/auth.js';
 import * as userController from '../controller/auth.js';
@@ -7,39 +7,39 @@ import * as userController from '../controller/auth.js';
 
 const userRouter = express.Router();
 
-const validateRegister = [
-    body('username')
-        .trim()
-        .notEmpty()
-        .withMessage('username을 입력해주세요.'),
-    body('password')
-        .trim()
-        .isLength({ min: 6 })
-        .withMessage('비밀번호는 최소 5글자입니다.'),
-    body('email')
-        .trim()
-        .isEmail()
-        .normalizeEmail()
-        .withMessage('이메일으로 입력해주세요.'),
-    // validate
-]
+// const validateRegister = [
+//     body('username')
+//         .trim()
+//         .notEmpty()
+//         .withMessage('username을 입력해주세요.'),
+//     body('password')
+//         .trim()
+//         .isLength({ min: 6 })
+//         .withMessage('비밀번호는 최소 5글자입니다.'),
+//     body('email')
+//         .trim()
+//         .isEmail()
+//         .normalizeEmail()
+//         .withMessage('이메일으로 입력해주세요.'),
+//     // validate
+// ]
 
-const validateFindPassword = [
-    body('password')
-        .trim()
-        .notEmpty()
-        .withMessage('바꾸실 비밀번호를 입력해주세요.'),
-    // validate
-]
+// const validateFindPassword = [
+//     body('password')
+//         .trim()
+//         .notEmpty()
+//         .withMessage('바꾸실 비밀번호를 입력해주세요.'),
+//     // validate
+// ]
 
 
 userRouter.route('/')
     .get(async (req, res, next) => {
         res.send(await userRepository.getUsers());
     });
-userRouter.route('/login').post(validate, userController.login);
-userRouter.route('/register').post(validateRegister, userController.createUser);
-userRouter.route('/:name/password').post(validateFindPassword, userController.changePassword);
+userRouter.route('/login').post( userController.login);
+userRouter.route('/register').post(userController.createUser);
+userRouter.route('/:name/password').post(userController.changePassword);
 
 
 
