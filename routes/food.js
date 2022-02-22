7ac3458ecb88270b4de69c9a307d1afc396c81c8
JaeHun Kim <kkaa81@naver.com>
2022-02-22 20:49:29 +0900

    delete get tag list

--- routes/food.js
+++ routes/food.js
@@ -6,8 +6,6 @@ const foodRouter = express.Router();
 
 foodRouter.route('/')
     .get(foodController.getStore)
-    .post(foodController.findStore) //선택한 음식 빼고 나머지 음식점 보내주기.
-foodRouter.route('/tag').get(foodController.getTags); // 모든 tag들 보내주기.
 foodRouter.route('/api/name').get(foodController.getFoodName);
 
 export default foodRouter;
