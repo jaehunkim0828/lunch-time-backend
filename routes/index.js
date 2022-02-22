6faa3fd11b11d6cdc7279b05fbb4d558cae4cb14
JaeHun Kim <kkaa81@naver.com>
2022-02-22 20:49:02 +0900

    create api router

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ import swaggerUi from 'swagger-ui-express';
 import userRouter from './auth.js';
 import foodRouter from './food.js';
 import options from './swagger.js';
+import apiRouter from './api/index.js';
 
 const rootRouter = express.Router();
 const specs = swaggerJSDoc(options);
 
 rootRouter.use('/auth', userRouter);
 rootRouter.use('/food', foodRouter);
-rootRouter.use('/', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true}))
+rootRouter.use('/api', apiRouter);
+rootRouter.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true}));
 
 
 export default rootRouter;
