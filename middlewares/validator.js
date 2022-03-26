3a31d352169b84ece58349eedcc9e7ff2c5628c4
JaeHun Kim <kkaa81@naver.com>
2022-03-26 15:45:23 +0900

    feat(store): add store validator

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,8 +2,9 @@ import { validationResult } from 'express-validator';
 
 export const validate = (req, res, next) => {
   const errors = validationResult(req);
+  const { body } = req;
   if (errors.isEmpty()) {
     return next();
   }
-  return res.status(400).json({ message: errors.array()[0].msg });
+  return res.status(400).json({ message: errors.array()[0].msg, body });
 };
--- routes/api/store.js
+++ routes/api/store.js
@@ -1,9 +1,33 @@
 import express from 'express';
+import { body } from 'express-validator';
 
 import * as storeRepository from '../../controller/api/store.js';
+import { validate } from '../../middlewares/validator.js';
+
+const validatestore = [
+    body('x')
+        .trim()
+        .isString()
+        .notEmpty()
+        .withMessage('x좌표가 안넘어왔습니다.'),
+    body('y')
+        .trim()
+        .notEmpty()
+        .withMessage('y좌표가 안넘어 왔습니다.'),
+    body('selected')
+        .isArray()
+        .withMessage('selected는 array이여야합니다'),
+    body('page')
+        .trim()
+        .notEmpty()
+        .isInt()
+        .withMessage('page가 안넘어왔습니다.'),
+    validate
+];
+
 
 const storeRouter = express.Router();
 
-storeRouter.route('/').post(storeRepository.findStore);
+storeRouter.route('/').post(validatestore, storeRepository.findStore);
 
 export default storeRouter;
